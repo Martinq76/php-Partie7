bcf3673a5b447cfbedf5854e181fe0ee9efb9d78
Martinq76 <martin.queguiner.42@gmail.com>
2019-05-23 10:49:21 +0200

    exo1

---
+++ exercice1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Exercice 1</title>
+    </head>
+    <body>
+        <form method="get" action="user.php">
+            <label for="lastname">Nom : </label>
+            <input type="text" name="lastname" id="lastname" />
+            
+            <label for="firstname">Prénom : </label>
+            <input type="text" name="firstname" id="firstname" />
+            
+            <input type="submit" value="Envoyer" />
+        </form>
+    </body>
+</html>
